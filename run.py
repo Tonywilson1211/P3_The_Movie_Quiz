b7a0b4ab5eeba8d1d25e9d49a1fbea9b83d3e290
Tonywilson1211 <ant.wilson@hotmail.com>
2023-03-07 19:55:12 +0000

    add print clue function

--- run.py
+++ run.py
@@ -35,6 +35,30 @@ def load_questions():
     return questions
 
 
+def get_clue_choice():
+    """
+    Get the user's choice on whether they want a clue.
+    """
+    error_message = ""
+    while True:
+        clue_choice = input(f"\033[F\033[K{error_message} \nWould "
+                            "you like a clue? (Y or N): ").strip().upper()
+        if clue_choice not in ['Y', 'N']:
+            error_message = "\033[F\033[KInput not recognised. "\
+                            "Please enter 'Y' or 'N'"
+        else:
+            error_message = ""
+            break
+    return clue_choice
+
+
+def print_question_clue(question):
+    """
+    Prints the clue for the user when they ask for a clue.
+    """
+    print_slowly(f"\nClue: {question['clue']}\n\n\n")
+
+
 def get_user_name():
     """
     Get user name
